056b51b7f79ac97f2ab2590ba19875502bcfb51e
austinbrown-opspark <austinworb@gmail.com>
2020-08-18 19:54:08 -0500

    Added new page for filtering :x

--- client/component/Choice.jsx
+++ client/component/Choice.jsx
@@ -3,7 +3,7 @@ import { Link, Route } from 'react-router-dom';
 import axios from 'axios';
 import { Row, Col } from "react-bootstrap";
 
-function Choice({ open, sessUser, sessDog, dogViews, allDogs, getFriends, index, setIndex, dogDisplayInfo, setDogDisplayInfo }) {
+function Choice({ open, sessUser, sessDog, dogViews, displayDogs, getFriends, index, setIndex, dogDisplayInfo, setDogDisplayInfo }) {
 
   const [ dogDisplay, setDogDisplay ] = useState('');
 
@@ -11,11 +11,19 @@ function Choice({ open, sessUser, sessDog, dogViews, allDogs, getFriends, index,
     setDogDisplay(dogViews[0]);
   }, [dogViews]);
 
-  const dislike = () => {
+  const like = (result) => {
+    // axios.post('/like', {
+    //   result, // boolean true/false
+    //   dogOwnerId: allDogs[index].id_user,
+    //   userId: 'placeholder for current logged in User ID'
+    // })
+    // .then((response) => {
+    //   // response should have bool if user was a match (if exists an entry in likes table that shows the dogOwnerID liked current user ID)
+    // })
     setIndex(index + 1);
     if (index < dogViews.length) {
       setDogDisplay(() => dogViews[index]);
-      setDogDisplayInfo(allDogs[index]);
+      setDogDisplayInfo(displayDogs[index]);
     } else {
       setDogDisplay(<div id='choice-box'><div id='alt'>Looks like you've made it through all the dogs in you're area. Please check back later.</div></div>);
     }
@@ -48,10 +56,10 @@ function Choice({ open, sessUser, sessDog, dogViews, allDogs, getFriends, index,
         </Row>
         <Row id='select'>
           <Col>
-            <button id='no' onClick={dislike}>No</button>
+            <button id='no' onClick={() => like(false)}>No</button>
           </Col>
           <Col>
-            <button id='yes' onClick={dislike}>Yes</button>
+            <button id='yes' onClick={() => like(true)}>Yes</button>
           </Col>
         </Row>
       </div>
